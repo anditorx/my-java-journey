81b6784e58e1925f975ff2ccfea1a8cfa190e73d
Andi Rustianto <anditodev46@gmail.com>
2022-03-22 01:19:37 +0700

    update learn java oop

---
+++ 03-java-oop/src/PersonApp.java
@@ -0,0 +1,16 @@
+/**
+ * Object adalah hasil instansiasi dari sebuah class
+ * Untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama Class dan kurung ()
+ * */
+public class PersonApp {
+    public static void main(String[] args) {
+        var person1 = new Person();
+        var person2 = new Person();
+        var person3 = new Person();
+
+
+        System.out.println(person1);
+        System.out.println(person2)
+        System.out.println(person3);;
+    }
+}
